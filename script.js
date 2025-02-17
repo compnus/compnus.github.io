b058d7012abe09fb68b50dc239c3992cc7b200cc
Stanley <stano5340@gmail.com>
2025-02-17 16:32:38 +0100

    im stupid

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ async function signUpUser(email, password) {
 
     if (error) {
         console.error("Sign-up error:", error.message);
-        return data, error;
+        return { data, error };
     }
 
-    return data, error;
+    return { data, error };
 }
 
 async function handlePostVerification() {
