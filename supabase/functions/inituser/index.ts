cd1139c460b1af176845af0889db10bd66457bbf
Stanley <stano5340@gmail.com>
2025-03-27 17:57:32 +0100

    that was a terrible idea

--- supabase/functions/inituser/index.ts
+++ supabase/functions/inituser/index.ts
@@ -1,6 +1,6 @@
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
-import { corsHeaders } from "../../_shared/cors.ts';
+import { corsHeaders } from "../_shared/cors.ts';
 
 Deno.serve(async (req) => {
     const supabase = createClient(
