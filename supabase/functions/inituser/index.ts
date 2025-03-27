1b366ee911faa50563d8fb76849c1cd9d77fe5a3
Stanley <stano5340@gmail.com>
2025-03-27 18:35:23 +0100

    desperate attempt

--- supabase/functions/inituser/index.ts
+++ supabase/functions/inituser/index.ts
@@ -64,7 +64,7 @@ Deno.serve(async (req) => {
         const { data: userExists, error: userExistsError } = await supabase
             .from("users")
             .select("id")
-            .eq("id", uid)
+            .eq("id", supabase.rpc('uuid',uid))
             .single();
 
         if (userExistsError || !userExists) {
