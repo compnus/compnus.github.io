dff10f4ebf03d90d6dd44691cda18323d8ed099a
Stanley <stano5340@gmail.com>
2025-04-16 15:23:24 +0200

    rightieho

--- supabase/functions/getbalance/index.ts
+++ supabase/functions/getbalance/index.ts
@@ -80,7 +80,7 @@ Deno.serve(async (req) => {
             });
         }
 
-        return new Response(JSON.stringify({ response: "User data processed successfully.", balance: balance }), {
+        return new Response(JSON.stringify({ response: "User data processed successfully.", balance: balance.balance_nus }), {
             status: 200,
             headers: {
                 ...headers
