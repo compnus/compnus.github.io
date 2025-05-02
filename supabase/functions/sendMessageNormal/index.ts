9ebd32b32bbefbd9fa7917993bee3d32738f2695
Stanley <stano5340@gmail.com>
2025-05-02 12:26:13 +0200

    okay let us see this sht

---
+++ supabase/functions/sendMessageNormal/index.ts
@@ -0,0 +1,153 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
+
+Deno.serve(async (req) => {
+    const supabase = createClient(
+        Deno.env.get('SUPABASE_URL'),
+        Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'),
+    );
+
+    const headers = { ...corsHeaders };
+
+    if (req.method === 'OPTIONS') {
+        return new Response(null, {
+            status: 204,
+            headers: {
+                ...headers
+            }
+        });
+    }
+
+    const authHeader = req.headers.get('authorization');
+    if (!authHeader) {
+        return new Response(JSON.stringify({ response: 'Authorization header missing' }), {
+            status: 401,
+            headers: {
+                ...headers
+            }
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+    const { data: user, error } = await supabase.auth.getUser(token);
+    if (error || !user) {
+        return new Response(JSON.stringify({ response: 'Invalid JWT' }), {
+            status: 401,
+            headers: {
+                ...headers
+            }
+        });
+    }
+
+    let uid: string | null = null;
+    let from: string | null = null;
+    let to: string | null = null;
+    let title: string | null = null;
+    let message: string | null = null;
+
+    try {
+        const body = await req.json();
+        uid = body.uid || null;
+        to = body.to || null;
+        title = body.title || null;
+        message = body.message || null;
+    } catch (error) {
+        console.error("Failed to parse JSON body", error);
+        return new Response(JSON.stringify({ response: "Failed to parse request body" }), {
+            status: 400,
+            headers: {
+                ...headers
+            }
+        });
+    }
+
+    if (!uid) {
+        return new Response(JSON.stringify({ response: "UID is required" }), {
+            status: 400,
+            headers: {
+                ...headers
+            }
+        });
+    }
+
+    try {
+        const { data: recuser, error: userExistsErrorn } = await supabase
+            .from("users")
+            .select("username")
+            .eq("id", uid)
+            .single();
+
+        if (userExistsErrorn || !recuser) {
+            return new Response(JSON.stringify({ response: `User ${uid} does not exist in the 'users' table.` }), {
+                status: 400,
+                headers: {
+                    ...headers
+                }
+            });
+        } else {
+            from = recuser.username;
+        }
+
+        let upmessage: string = `
+    %$t%${title}%$,$
+    %$f%${from}%$,%
+    %$m%${message.split("\n").join("<br>")}
+    %$$%
+        `;
+
+        const { data: senuser, error: userExistsError } = await supabase
+            .from("users")
+            .select("messages")
+            .eq("username", to)
+            .single();
+
+        if (userExistsError || !senuser) {
+            return new Response(JSON.stringify({ response: `User ${to} does not exist.` }), {
+                status: 404,
+                headers: {
+                    ...headers
+                }
+            });
+        } else {
+            const { error: cannotSend } = await supabase
+                .from("users")
+                .update("messages", upmessage + senuser.messages)
+                .eq("username", to);
+            if (cannotSend) {
+                return new Response(JSON.stringify({ response: `Could not send message to ${to} due to error: ${cannotSend}` }), {
+                    status: 401,
+                    headers: {
+                        ...headers
+                    }
+                });
+            }
+            const { error: logError } = await supabase
+                .from("logs")
+                .insert([{ created_by: from, type: "normalMessage", attributes: "to->"+to, message: "title->"+title+"\ncontent->"+message]);
+            if (logError) {
+                return new Response(JSON.stringify({ response: `Internal server error.` }), {
+                    status: 500,
+                    headers: {
+                        ...headers
+                    }
+                });
+            }
+        }
+
+        return new Response(JSON.stringify({ response: "Message sent successfully!" }), {
+            status: 200,
+            headers: {
+                ...headers
+            }
+        });
+    } catch (error) {
+        console.error("Error processing request", error);
+        return new Response(JSON.stringify({ response: "Internal Server Error." }), {
+            status: 500,
+            headers: {
+                ...headers
+            }
+        });
+    }
+});
--- u/message.js
+++ u/message.js
@@ -1,4 +1,7 @@
 var selected = 0;
+var lastLoc = 0;
+var selectStyle = 0;
+var dt = { user_id: null, uid: null };
 
 function messageType(type) {
     selected = type;
@@ -6,6 +9,29 @@ function messageType(type) {
     document.getElementById("type" + type).classList.add("selected");
     document.getElementById("types" + Number(!type)).classList.remove("selected");
     document.getElementById("type" + Number(!type)).classList.remove("selected");
+    loadNocas();
+}
+
+async function loadNocas() {
+    const { data, error } = await supabase.auth.getUser();
+    dt.user_id = data.user.id;
+    dt.uid = (await supabase.auth.getSession()).data.session?.user.id;
+    await fetch('https://jwpvozanqtemykhdqhvk.supabase.co/functions/v1/getbalance', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${(await supabase.auth.getSession()).data.session?.access_token}`
+        },
+        body: JSON.stringify(dt)
+    })
+        .then(response => response.json())
+        .then(data => {
+            y = data.balance[1];
+            document.getElementById("nocabalance").innerHTML = y;
+        })
+        .catch((error) => {
+            console.error('Error invoking function:', error);
+        });
 }
 
 async function sendMessage() {
@@ -16,15 +42,118 @@ function remainingChar() {
     var rm = document.getElementById("remainingchar");
     var length = 300 - document.getElementById("normalmsg").value.length;
     var breaks = document.getElementById("normalmsg").value.split("\n").length - 1;
-    var length = length-(breaks*4);
+    var lefts = document.getElementById("normalmsg").value.split("<").length - 1;
+    var rights = document.getElementById("normalmsg").value.split(">").length - 1;
+    var amps = document.getElementById("normalmsg").value.split("&").length - 1;
+    length = length - ((breaks + amps) * 4) - ((lefts + rights) * 3);
     rm.innerHTML = "Characters remaining: " + length;
     if (length >= 100) rm.style.color = "white";
     else if (length >= 50) rm.style.color = "yellow";
     else if (length >= 0) rm.style.color = "orange";
-    else rm.style.color = "red";
+    else rm.style.color = "orangered";
     var x = document.getElementById("limitedsend");
     if (length >= 0) x.classList.remove("disabled");
     else x.classList.add("disabled");
+}
+
+function increaseChar() {
+    var advmsg = document.getElementById("advmsg").value;
+    var rm = document.getElementById("charcount");
+    var length = advmsg.length;
+    var breaks = advmsg.split("\n").length - 1;
+    var lefts = advmsg.split("<").length - 1;
+    var rights = advmsg.split(">").length - 1;
+    var amps = advmsg.split("&").length - 1;
+    var lbr = advmsg.split("{{").length - 1;
+    var rbr = advmsg.split("}}").length - 1;
+    var hrs = advmsg.split("{l}").length - 1;
+    var hs = advmsg.split("{h}").length - 1;
+    var nhs = advmsg.split("{/h}").length - 1;
+    var quts = advmsg.split('"').length - 1;
+    var links = advmsg.split("{link}").length - 1;
+    var imgs = advmsg.split("{img}").length - 1;
+    var nbsps = advmsg.split("{!}").length - 1;
+    var imgsts = (advmsg.split("{img1}").length - 1) + (advmsg.split("{img2}").length - 1) + (advmsg.split("{img3}").length - 1) + (advmsg.split("{img4}").length - 1) + (advmsg.split("{img5}").length - 1) + (advmsg.split("{img6}").length - 1) + (advmsg.split("{img7}").length - 1) + (advmsg.split("{img8}").length - 1);
+    length = length + ((breaks + amps) * 4) + ((lefts + rights + nbsps) * 3) + ((lbr + rbr) * 6) + (hs+nhs) + quts*5 + (imgs*2) + (imgsts*15) + hrs*8 + links*30;
+    rm.innerHTML = "Characters: " + length;
+    document.getElementById("messagecost").innerHTML = 1 + links + (Math.floor((length - 1) / 50) >= 0 ? Math.floor((length - 1) / 50):0);
+}
+
+function preview() {
+    var content = document.getElementById("advmsg").value.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\n", "<br>").replaceAll("{l}<br>", "{l}")
+        .replaceAll("&", "&amp;").replaceAll("{{", "&lbrace;").replaceAll("}}", "&rbrace;").replaceAll("{b}", "<b>").replaceAll("{/b}", "</b>").replaceAll("{i}", "<i>").replaceAll("{/i}", "</i>").replaceAll("{u}", "<u>").replaceAll("{/u}", "</u>")
+        .replaceAll("{s}", "<s>").replaceAll("{/s}", "</s>").replaceAll("{h}", "<h0>").replaceAll("{/h}", "</h0>").replaceAll("{l}", "</p><hr><p>").replaceAll("{link}", "<a class='linkstv' target='_blank' href='").replaceAll("{text}", "'>")
+        .replaceAll("{/link}", "</a>")        .replaceAll("{/img}", "'>").replaceAll("{img}", "<img src='").replaceAll("{img1}", "<img class='img1' src='").replaceAll("{img2}", "<img class='img2' src='").replaceAll("{img3}", "<img class='img3' src='")
+        .replaceAll("{img4}", "<img class='img4' src='").replaceAll("{img5}", "<img class='img5' src='").replaceAll("{img6}", "<img class='img6' src='").replaceAll("{img7}", "<img class='img7' src='").replaceAll("{img8}", "<img class='img8' src='")
+        .replaceAll("{!}","&nbsp;");
+    popup("Preview", `</p>
+        <div class="msgmsgmain">
+        <div class="msgmsg">
+        <p>
+        ${content}
+        </p>
+        </div>
+        </div>
+        <p style="margin:0">
+    `);
+}
+
+function add(x) {
+    var msg = document.getElementById("advmsg");
+    lastLoc = msg.selectionEnd;
+    msg.value = msg.value.slice(0, lastLoc) + x + msg.value.slice(lastLoc);
+    msg.selectionEnd = lastLoc + x.length;
+    msg.focus();
+}
+
+function addLink() {
+    popup("Add Link", `
+        </p>
+        <div class="input">
+            <label for="llink">URL:</label>
+            <input id="llink" placeholder="https://example.com" oninput='this.value=this.value.replaceAll(" ","%20").replaceAll("<","%3C").replaceAll(">","%3E").replaceAll("{","%7B").replaceAll("}","%7D").replaceAll("\\"","%22")'/>
+        </div>
+        <div class="input">
+            <label for="ltext">Displayed Text:</label>
+            <input id="ltext" placeholder="My Link!"/>
+        </div>
+        <div class="flex cc"><button onclick="addLinkComp('popup${popupid}')">Add Link</button></div><p style="margin:0">
+    `);
+}
+
+function addLinkComp(id) {
+    add(`{link}${document.getElementById("llink").value}{text}${document.getElementById("ltext").value}{/link}`);
+    document.getElementById(id).style.opacity = 0; window.setTimeout(() => document.body.removeChild(document.getElementById(id)), 201);
+}
+
+function addImage() {
+    selectStyle = 0;
+    popup("Add Image", `
+        </p>
+        <div class="input">
+            <label for="ilink">Link to Image:</label>
+            <input id="ilink" placeholder="https://example.com" oninput='this.value=this.value.replaceAll(" ","%20").replaceAll("<","%3C").replaceAll(">","%3E").replaceAll("{","%7B").replaceAll("}","%7D").replaceAll("\\"","%22")'/>
+        </div>
+        <div class="input"><label>Style:</label></div>
+        <div id="ichoser">
+            <div class="choserthing" style="grid-row:1/3"><div id="choser0" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=0; this.classList.add('chosen')" class="choser chosen" title="Default"></div></div>
+            <div class="choserthing"><div id="choser1" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=1; this.classList.add('chosen')" class="choser" title="Square (Big)"></div></div>
+            <div class="choserthing"><div id="choser2" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=2; this.classList.add('chosen')" class="choser" title="Square (Small)"></div></div>
+            <div class="choserthing"><div id="choser3" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=3; this.classList.add('chosen')" class="choser" title="Rounded (Big)"></div></div>
+            <div class="choserthing"><div id="choser4" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=4; this.classList.add('chosen')" class="choser" title="Rounded (Small)"></div></div>
+            <div class="choserthing"><div id="choser5" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=5; this.classList.add('chosen')" class="choser" title="Circle (Big)"></div></div>
+            <div class="choserthing"><div id="choser6" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=6; this.classList.add('chosen')" class="choser" title="Circle (Small)"></div></div>
+            <div class="choserthing"><div id="choser7" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=7; this.classList.add('chosen')" class="choser" title="Hover (Big)"><p>HOVER*</p></div></div>
+            <div class="choserthing"><div id="choser8" onclick="document.querySelector('.chosen').classList.remove('chosen'); selectStyle=8; this.classList.add('chosen')" class="choser" title="Hover (Small)"><p>HOVER*</p></div></div>
+        </div>
+        <p>* Note: On mobile devices, the effect will activate upon clicking.</p>
+        <div class="flex cc"><button onclick="addImageComp('popup${popupid}')">Add Image</button></div><p style="margin:0">
+    `);
+}
+
+function addImageComp(id) {
+    add(`{img${selectStyle===0?"":selectStyle}}${document.getElementById("ilink").value}{/img}`);
+    document.getElementById(id).style.opacity = 0; window.setTimeout(() => document.body.removeChild(document.getElementById(id)), 201);
 }
 
 function fixTitle(which) {
