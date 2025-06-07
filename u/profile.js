1bd25269a85888b700951d7676956e09b1ef7c2a
Stanley <stano5340@gmail.com>
2025-06-07 14:24:38 +0200

    test

--- u/profile.js
+++ u/profile.js
@@ -51,7 +51,10 @@ async function loadWallet() {
     wholes = Math.floor(y);
     rems = Math.round((y - wholes) * 10000);
     document.getElementById("walletsats").innerHTML = `${wholes}<span class="walletdecimal">.${"0".repeat(4 - rems.toString().length)}${rems}</span>`;
- 
+    var pricebtc;
+    fetch('https://data-api.coindesk.com/index/cc/v1/latest/tick?market=ccix&instruments=BTC-USD').then(response => response.json()).then(json => { pricebtc = json.Data["BTC-USD"].VALUE });
+    var pricebtcnew = (y * (pricebtc / 100000000)).toFixed(3);
+    document.getElementById("valuebtcsats").innerHTML = `&asymp; $${pricebtcnew.substring(0, 4)}<span style="opacity:0.3">${pricebtcnew.substring(4)}</span>`;
 }
 
 async function loadMessages() {
