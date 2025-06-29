6528e4d5dea8c1627265aafce02db80aa4f5fac0
Stanley <stano5340@gmail.com>
2025-06-29 18:07:18 +0200

    test

---
+++ u/wallet.js
@@ -0,0 +1,11 @@
+async function loadWallet() {
+    balance = await getBalance((await supabase.auth.getSession()).data.session?.user.id);
+    document.getElementById("walletnus").innerHTML = balance[0];
+    document.getElementById("walletnoca").innerHTML = balance[1];
+    document.getElementById("walletsats").innerHTML = balance[2];
+    var pricebtc;
+    await fetch('https://data-api.coindesk.com/index/cc/v1/latest/tick?market=ccix&instruments=BTC-USD').then(response => response.json()).then(json => { pricebtc = json.Data["BTC-USD"].VALUE });
+    var pricebtcnew = (balance[2] * (pricebtc / 100000000)).toFixed(3);
+    document.getElementById("bitcoinss").innerHTML = (balance[2] / 100000000).toFixed(8);
+    document.getElementById("bitcoinpr").innerHTML = pricebtcnew;
+}
