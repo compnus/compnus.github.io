c7a0ceb358bc1e6fb654be7fe8044f113b9cdcd4
Stanley <stano5340@gmail.com>
2025-07-03 18:46:50 +0200

    hgfhgdhgfdfhgd

--- head.js
+++ head.js
@@ -17,10 +17,25 @@ const head = `
 document.head.innerHTML += head;
 
 const header = document.createElement("template");
+const promoheader = document.createElement("template");
 const footer = document.createElement("template");
 const fullfooter = document.createElement("template");
 
 header.innerHTML = `
+<header>
+<div class="flex cc">
+<a href="/"><img id="headerlogo" src="/site/image/logo/main.svg"/></a>
+<h1 id="title"></h1>
+</div>
+<div id="headernav">
+<p class="link hnav" id="cryptonus" title="Free crypto, new crypto projects, scam list and so much more!"><a href="/crypto/">CryptoNUS</a></p>
+<p class="link hnav" id="servicenus" title="Projects, tools and other services by CompNUS, or by partners of CompNUS."><a href="/services.html">Services</a></p>
+<p class="link hnav" id="userlogin" title="Gateway to everything NUS: mine $NUS crypto and more!"><a href="/u/signup.html">Sign Up</a> <span style="color: white !important">/</span> <a href="/u/login.html">Log In</a></p>
+</div>
+</header>
+`;
+
+promoheader.innerHTML = `
 <header>
 <div class="flex cc">
 <a href="/"><img id="headerlogo" src="/site/image/logo/main.svg"/></a>
@@ -61,6 +76,32 @@ fullfooter.innerHTML = `
 function initHeader(title) {
     document.body.appendChild(header.content);
     document.getElementById("title").innerHTML = title;
+    document.addEventListener("DOMContentLoaded", async () => {
+        var user = await getUser();
+        var usern;
+        if (user.user) {
+            const { data: username, error: dbError } = await supabase
+                .from("users")
+                .select("username")
+                .eq("id", user.data.id)
+                .single();
+
+            if (dbError) {
+                console.log(dbError);
+            } else {
+                if (username.username) usern = username.username;
+            }
+            document.getElementById("userlogin").innerHTML = `<a href="/u/profile.html">My Profile (${usern})</a>`;
+        }
+    });
+}
+
+function promoHeader(title, color, image) {
+    document.body.appendChild(header.content);
+    document.getElementById("title").innerHTML = title;
+    document.getElementById("headerlogo").src = image;
+    document.getElementById("headerlogo").style.borderRadius = "10%";
+    document.querySelector("header").style.background = color;
     document.addEventListener("DOMContentLoaded", async () => {
         var user = await getUser();
         var usern;
