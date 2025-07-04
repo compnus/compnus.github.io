30b2a35db16d811ca0cd4d573102dc1e7e522ed6
Stanley <stano5340@gmail.com>
2025-07-04 12:11:39 +0200

    ffdgdgfdfdgfdfggggggggggggggggggg

--- u/inventory.js
+++ u/inventory.js
@@ -1,5 +1,5 @@
 async function initInventory(fr) {
-    const { data, error } = await supabase.from("udata").select("inventory").eq("user_id", fr.user.id).single();
+    const { data, error } = await supabase.from("udata").select("inventory").eq("user_id", fr.id).single();
     if (!data || error) document.getElementById("infotext").innerHTML = "Something went wrong.";
     else document.getElementById("infotext").innerHTML = "Your inventory is empty.";
 }
