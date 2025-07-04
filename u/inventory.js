725e52fda228f82ddadb7969ec13781c7b538704
Stanley <stano5340@gmail.com>
2025-07-04 12:07:53 +0200

    okay im done

--- u/inventory.js
+++ u/inventory.js
@@ -1,3 +1,5 @@
-function initInventory(fr) {
-    console.log(fr);
+async function initInventory(fr) {
+    const { data, error } = await supabase.from("udata").select("inventory").eq("user_id", fr.user.id).single();
+    if (!data || error) document.getElementById("infotext").innerHTML = "Something went wrong.";
+    else document.getElementById("infotext").innerHTML = "Your inventory is empty.";
 }
