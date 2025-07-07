26db24f424cb1c65d6b08546bde01eadaf0fd401
Stanley <stano5340@gmail.com>
2025-07-07 17:06:45 +0200

    uhhhh

---
+++ crypto/crypto.js
@@ -0,0 +1,9 @@
+function search(via) {
+
+}
+
+
+
+
+
+console.log("list loaded");
--- u/inventory.js
+++ u/inventory.js
@@ -1,3 +1,7 @@
+const ITEMS = {
+
+};
+
 async function initInventory(fr) {
     const { data, error } = await supabase.from("udata").select("inventory").eq("user_id", fr.id).single();
     if (!data || error) document.getElementById("infotext").innerHTML = "Something went wrong.";
