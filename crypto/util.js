1aa8d67b74e74274e116ed083ad9c0b259093dbe
Stanley <stano5340@gmail.com>
2026-01-30 12:42:21 +0100

    push

--- crypto/util.js
+++ crypto/util.js
@@ -21,7 +21,18 @@ function submitForm(page, screen) {
     status.innerHTML = "Please wait...";
     status.style.display = "block";
     button.classList.add("disabled");
+    var body = {uid: getUser().data.id};
     if (page == "add") {
-
+        body.name = document.getElementById(screen + "name").value;
+        body.link = document.getElementById(screen + "link").value;
+        body.type = screen;
+        if (screen != "scam") {
+            body.links = {
+                web: document.getElementById(screen + "lw").value,
+                android: document.getElementById(screen + "la").value,
+                ios: document.getElementById(screen + "li").value,
+                tg: document.getElementById(screen + "lt").value
+            }
+        }
     }
 }
