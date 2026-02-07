f36ddb71e13eeca7cfcab4a4e0683b3c49a0a011
Stanley <stano5340@gmail.com>
2026-02-07 18:33:44 +0100

    okay im done for today

--- crypto/listScam.js
+++ crypto/listScam.js
@@ -2,12 +2,20 @@ var FILTERID = "scam0";
 
 var LISTS = [
     {
-        name: "Test",
-        icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.bitgetimg.com%2FmultiLang%2Fweb%2F62e18cee66b35accc2bc058b443ebe34.png&f=1&nofb=1&ipt=3906f68d603756784f2c35345e24d61f662d7b519e652d1eb13211f48caa0827",
-        short: "An actual wallet by the way! Spell Wallet not only holds your tokens and stakes them (SOL only), but also allows you to earn MANA and participate in Missions to earn money!",
-        long: `Spell wallet allows you to <a class="link" onclick="popup('Token Staking in Spell Wallet', 'Sewat Wallet allows you to stake <b>Solana</b> from 0.001 SOL at up to 6.7% APY.')">hold and stake various tokens</a>, while also allowing you to collect MANA through mining, daily check-ins and quests and earn money (or more MANA) by participating in missions! You will be able to use MANA in the future to unlock airdrops!`,
-        id: "test",
-        category: 1
+        name: "Qzino",
+        icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcoingape.com%2Fwp-content%2Fuploads%2F2024%2F03%2FQzino-square-logo-2.webp&f=1&nofb=1&ipt=72bb292163f9b9b9b53b8e8a06e71f41507e564e32822f8ade5ac6871929664d",
+        short: "Now the casino may not be a scam, but the project that preceeded it definitely was.",
+        long: `So basically, Qzino now is a casino/betting app. We are not sure whether it is a scam or legit, but that is completely besides the point. Before it was a casino, it used to be an app for rolling Telegram Gifts. After migration to the casino, all deposited TON, Stars and collected USDT vouchers disappeared and in order to get them back you must wager instane amounts of money. They also have this free spins thing? Where you can pay money to get free spins? The money does not get you anything only the spins. Why call them free? Anyway, in order to get the money you earned from free spins you must wager way more. Yeah it is only getting worse. The main point is: this app betrayed its early supporters.`,
+        id: "qzino",
+        category: 2
+    },
+    {
+        name: "X-Market",
+        icon: "https://x-market.io/LOGO/XMT-logo.png",
+        short: "Make deposit their money and then turn the promised token into random leaderboard points worth nothing. Great job.",
+        long: `X-Market used to be a regular mining app, you would mine the $AIX token, hoping it would launch on TON and you would make some money. Later it did launch - not as anyone expected it to, but it did. People could not only withdraw but also deposit. Later, it turned into the betting up we know and hate today. All the money you invested into it is gone, there is a new token that you need to pay to even get airdrop access... It might not be a scam, but after the past expierience? Yeah try to avoid it.`,
+        id: "aix",
+        category: 2
     },
     {
         name: "SpinFi",
@@ -24,6 +32,46 @@ var LISTS = [
         long: `As much as this game is not that bad for a game - yes it gets boring after some time, but there is some charm - this app presents itself as a way to get money, which is certainly not possible. Basically you earn Miner Coins, which can be exchanged for $MINER crypto (or at least, it should be possible) which you can then withdraw as USDT. The only detail and perhaps an issue is that: it does not work? Attempting to exchange Miner Coins for $MINER yields a message saying "not enough gems", which makes no sense because there is no information about you needing gems to exchange. We messaged the official support about this and, as you could have guessed, we got no response. It has been over a year since we sent the email. That is another reason to think this app is a scam.`,
         id: "bitgame",
         category: 2
+    },
+    {
+        name: "Epic Gift",
+        icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftehnoobzor.com%2Fwp-content%2Fuploads%2F2025%2F09%2Fepic-gift-280037.png&f=1&nofb=1&ipt=dd972d4467a08624a4e2e49fe97dea8e9fe3d3a992cf820a70842d422ddab2fe",
+        short: "The red flags on this one are truly <i>epic</i>.",
+        long: `Epic Gift presents itself as an app to get free Telegram Gifts. Eventually, you need to deposit stars to continue rolling free TON. And that would not really be an issue, after all, hazard aspect is not a red flag, but the fact this requirement appears when a withdrawable gist is just a roll or two away really makes you rethink your life choices. Now of course, there are other red flags such as a beautiful deposit button being shoved into your face while the withdraw button is nowhere to be found.`,
+        id: "epicgiftbot",
+        category: 2
+    },
+    {
+        name: "&#1047;&#1074;&#1105;&#1079;&#1076;&#1099; &#1079;&#1072; &#1091;&#1084;",
+        icon: "https://dbc.fmsnsr.xyz/api/files/1nlpavfhdos0lje/eg16g369pxxvadd/avatar_ho51om5tbe.jpg",
+        short: "If you have any &#1091;&#1084;, you will know better than to waste your time with this one.",
+        long: `Stars for intelligence (&#1047;&#1074;&#1105;&#1079;&#1076;&#1099; &#1079;&#1072; &#1091;&#1084;) is a bot that really tests your intelligence. Because if you keep going even after you have joined 100 scam groups, you might have none. This app allows you to get 99 Stars for solving math equations. The minimum withdrawal threshold is 100 Stars. So how do you get the last one? Well it is very simple my friend! Just join our group! Oh okay, so I did that what now? Oh you want more groups? Sure here are ten more groups to join. Okay I joined them, can I get my stars now? Oh, of course! Just join these ten new groups! You get the point.`,
+        id: "zvjozdyzaum",
+        category: 1
+    },
+    {
+        name: "&#1047;&#1074;&#1077;&#1079;&#1076;&#1072;&#32;&#1087;&#1086;&#1076;&#32;&#1082;&#1085;&#1086;&#1087;&#1082;&#1086;&#1081;",
+        icon: "https://dbc.fmsnsr.xyz/api/files/1nlpavfhdos0lje/i3hbl88gb8h6aj2/avatar_1hu3buiv07.jpg",
+        short: "The only button you should press when interacting with this bot, is the delete and block button.",
+        long: `Star under the button they said. The only thing hiding under the button are ads. Oh also, same situation as <a class="link" href="?id=zvjozdyzaum">Stars for intelligence</a>, you can never withdraw.`,
+        id: "zvjezdapodknopkoj",
+        category: 1
+    },
+    {
+        name: "&#1047;&#1072;&#1073;&#1077;&#1088;&#1080;&#32;&#1087;&#1086;&#1076;&#1072;&#1088;&#1086;&#1082;",
+        icon: "https://botstat.io/files/images/@tg_podarkov_bot_1770455348.jpg",
+        short: "Yet another one of these 'get stars/gifts quickly' bots. Telegram is filled with these for some reason.",
+        long: `Just read this, I refuse to write it multiple times: <a class="link" href="?id=zvjozdyzaum">Stars for intelligence</a>.`,
+        id: "zaberipodarok",
+        category: 1
+    },
+    {
+        name: "kegli",
+        icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpixy.org%2Fsrc%2F294%2Fthumbs350%2F2941790.jpg&f=1&nofb=1&ipt=05e894678db10dc0d7ba2517d7989e65171b31e7f9f9607211e2b0e00c19b234",
+        short: "Want some stars? Well too bad!",
+        long: `Yet another one of those infinite ad loop holes like <a class="link" href="?id=zvjozdyzaum">Stars for intelligence</a>.`,
+        id: "kegli",
+        category: 1
     }
 ];
 
