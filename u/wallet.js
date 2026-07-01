ab2c6c25262bbdaf7c06e5e5647157224fc04194
Stanley <stano5340@gmail.com>
2026-07-01 11:59:31 +0200

    knjkjhksfd

--- u/wallet.js
+++ u/wallet.js
@@ -366,6 +366,26 @@ async function ttload(id) {
     }
 }
 
+function formatDate(udf) {
+    function getOrdinal(day) {
+        if (day > 3 && day < 21) return "th";
+        switch (day % 10) {
+            case 1: return "st";
+            case 2: return "nd";
+            case 3: return "rd";
+            default: return "th";
+        }
+    }
+    const date = new Date(udf);
+    const day = date.getUTCDate();
+    const month = date.toLocaleString("en-GB", {
+        month: "long",
+        timeZone: "UTC",
+    });
+    const year = date.getUTCFullYear();
+    return `${day}${getOrdinal(day)} ${month} ${year}`;
+}
+
 async function verifyTID(tid) {
     var status = document.getElementById("ttveri");
     var view = document.getElementById("ttverf");
@@ -389,6 +409,15 @@ async function verifyTID(tid) {
             if (data.response === '0') {
                 status.style.display = "none";
                 view.style.display = "block";
+                document.getElementById("ttv_id").innerHTML = data.data.id;
+                document.getElementById("ttv_date").innerHTML = formatDate(data.data.date);
+                document.getElementById("ttv_from").innerHTML = data.data.from;
+                document.getElementById("ttv_to").innerHTML = data.data.to;
+                document.getElementById("ttv_mes").innerHTML = data.data.message;
+                fillResources(assets, data.data.resource);
+                document.getElementById("ttid").value = "";
+            } else {
+                status.innerHTML = data.response;
             }
         })
         .catch((error) => {
