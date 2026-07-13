b16ef72b3a99a1a8319282987582e8928294e02b
Stanley <stano5340@gmail.com>
2026-07-13 18:08:47 +0200

    okie nus time

--- crypto/nus/script.js
+++ crypto/nus/script.js
@@ -1,3 +1,46 @@
-async function main() {
+var SIDES = 0;
+var uid = "";
 
+async function main() {
+    const { user, data } = await getUser();
+    //if (!user) window.location.href = "/u/login.html";
+    uid = data.id;
+
+    var balance = await getBalance(uid);
+    document.getElementById("walletnus").innerHTML = balance[0];
+    document.getElementById("walletnoca").innerHTML = balance[1];
+    document.getElementById("walletsats").innerHTML = balance[2];
 }
+
+function collapseSide(which) {
+    if (which == "left") {
+        if (SIDES % 2 == 1) SIDES -= 1;
+        else SIDES += 1;
+    } else {
+        if (SIDES >= 2) SIDES -= 2;
+        else SIDES += 2;
+    }
+    document.getElementById("collapser" + which).classList.toggle("collapsed");
+    var leftbar = document.getElementById("leftsd");
+    var rightbar = document.getElementById("rightsd");
+    var grid = document.querySelector("main");
+    switch (SIDES) {
+        case 0: //both visible
+            grid.style.gridTemplateColumns = "25vw 48vw 25vw";
+            leftbar.style.right = "0";
+            break;
+        case 1: //left hidden
+            grid.style.gridTemplateColumns = "4vh calc(73vw - 4vh) 25vw";
+            leftbar.style.right = "calc(25vw - 4vh)";
+            break;
+        case 2: //right hidden
+            grid.style.gridTemplateColumns = "25vw calc(73vw - 4vh) 4vh";
+            leftbar.style.right = "0";
+            break;
+        case 3: //both hidden
+            grid.style.gridTemplateColumns = "4vh calc(98vw - 8vh) 4vh";
+            leftbar.style.right = "calc(25vw - 4vh)";
+    }
+}
+
+console.log("nus loaded");
