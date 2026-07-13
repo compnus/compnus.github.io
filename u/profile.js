c516d29375ea62b4ed4f387453d68ae663fd1172
Stanley <stano5340@gmail.com>
2026-07-13 15:32:33 +0200

    okaaay push this pls

--- u/profile.js
+++ u/profile.js
@@ -318,7 +318,26 @@ I do acknowledge, that I will lose access to everything connected to my CompNUS 
 }
 
 async function showAccountInfo() {
-    const { data, error } = await sb.from("users").select("username, email").eq("id", dt.uid).single();
+    function formatDate(udf) {
+        function getOrdinal(day) {
+            if (day > 3 && day < 21) return "th";
+            switch (day % 10) {
+                case 1: return "st";
+                case 2: return "nd";
+                case 3: return "rd";
+                default: return "th";
+            }
+        }
+        const date = new Date(udf);
+        const day = date.getUTCDate();
+        const month = date.toLocaleString("en-GB", {
+            month: "long",
+            timeZone: "UTC",
+        });
+        const year = date.getUTCFullYear();
+        return `${day}${getOrdinal(day)} ${month} ${year}`;
+    }
+    const { data, error } = await sb.from("users").select("username, email, created").eq("id", dt.uid).single();
     const { data: data1, error: error1 } = await sb.from("udata").select("can_message, referred, invitees").eq("user_id", dt.uid).single();
     if (error || !data || error1 || !data1) {
         popup("An Error Occured", "An error occurred while fetching your account information.<br>" + (error ? error.message : "") + (error1 ? "<br>" + error1.message : ""));
@@ -330,6 +349,7 @@ async function showAccountInfo() {
     popup("Account Information", `
     <b>Username:</b> ${data.username}<br>
     <b>Email:</b> ${data.email}<br>
+    <b>Account created on </b>${formatDate(data.created)}<br>
     <b>Banned from Sending Messages:</b> ${data1.can_message ? "No" : "Yes"}<br>
     ${data1.referred ? "<b>Referred By:</b> " + data1.referred + "<br>" : ""}<br>
     You invited <b>${invites} user${invites == 1 ? "" : "s"}</b> to CompNUS.
