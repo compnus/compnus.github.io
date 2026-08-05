353dbcf10e4af04929cbbca1418da1f86a997261
Stanley <stano5340@gmail.com>
2026-08-05 14:25:53 +0200

    final touches

--- crypto/nus/script.js
+++ crypto/nus/script.js
@@ -305,7 +305,6 @@ async function miningStart() {
             } else if ((data.code === 2) || (data.code === 5)) {
                 response = JSON.parse(data.response);
                 serverdata.last_claimed = response.newtime;
-                console.log(response, serverdata);
                 popup("Mining rewards claimed!", `You have received ${response.reward} $NUS!<br>You may claim again once the timer expires.` + (data.code === 2 ? "<br><br>Due to an internal error, your claim will not show up in your transaction history." : ""));
                 const balance = await getBalance(uid);
                 document.getElementById("walletnus").innerHTML = balance[0];
