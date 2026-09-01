baccc4153a8eba9cbb103234c3b876447dba7155
Stanley <stano5340@gmail.com>
2026-09-01 18:00:57 +0200

    okay gn

--- crypto/nus/levels.js
+++ crypto/nus/levels.js
@@ -1,6 +1,7 @@
 var uid = "";
 var serverdata;
 var LEVELS;
+var __shadowl = 0;
 
 async function main() {
     const { user, data } = await getUser();
@@ -33,6 +34,7 @@ async function loadData() {
         document.getElementById('progressbar').style.display = 'none';
         document.getElementById('levelinfo').style.display = 'none';
         document.getElementById('level_up').innerHTML = 'MAX LEVEL';
+        document.getElementById('information').innerHTML = "Stay tuned for more levels!"
     } else {
         var completion = serverdata.exp / maxxp;
         completion = Math.min(1, completion);
@@ -55,5 +57,58 @@ function levelPage(turn) {
 }
 
 async function levelUp() {
-
+    startLoading();
+    if (serverdata.balance_noca < 10) {
+        stopLoading();
+        popup("Not Enough Nocas!", "You need 10 Nocas to level up!<br>Get Nocas for free <a class='link' href='free.html'>here</a> or head over to your <a class='link' href='../../u/wallet.html'>wallet</a> and get Nocas by exchanging them for $NUS or Bitcoin Satoshi.", true, true);
+        return;
+    }
+    if (serverdata.level >= 10) {
+        if (__shadowl<7)stopLoading();
+        switch (__shadowl) {
+            case 0:
+                popup("Why?", "You are on the max level already.");
+                break;
+            case 1:
+                popup("...", "Alright we get it, you can use the browser console. Have fun.");
+                break;
+            case 2:
+                popup("...", "Seriously what are you trying to achieve?");
+                break;
+            case 3:
+                popup("", "I mean, have fun.");
+                break;
+            case 4:
+                popup("", "Actually, good luck with this one.", false);
+                break;
+            case 5:
+                popup("", "Did I even tell you to close these in the first place? No I didn't. Did I?", false);
+                break;
+            case 6:
+                popup("", "I have a better idea.", false);
+                break;
+            case 7:
+                break;
+            case 8:
+                popup("", loadingid?"It do be loading. Be patient. It might end. Someday.":"Okay yea you know how the console works. Great.", false);
+                break;
+            case 9:
+                popup("", loadingid?":D":"Henceforth I'm too bored to continue writing these.", false);
+                break;
+            case 10:
+                popup("", loadingid ? "Bro ditch the loading dialog it's annoying." : "Still, was fun to add something pointless like this.", false);
+                break;
+            default:
+                popup("", ":D", false);
+                break;
+        }
+        __shadowl++;
+        return;
+    }
+    if (serverdata.exp < LEVELS.perks[serverdata.level + 1][0]) {
+        stopLoading();
+        popup("Not enough XP!", "It would be ideal if you stayed on the 'common user' side of this website to prevent issues like this one. Not trying to be offensive, just afraid something will break catastrophically.", true, true);
+        return;
+    }
+    //level up logic
 }
